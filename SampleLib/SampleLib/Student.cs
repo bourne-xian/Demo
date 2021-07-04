4f6c2c2b72132e9dca6a201ec510222e04b29783
bourne.xian <bourne.xian@outlook.com>
2021-07-04 23:22:14 +0800

    Add SampleLib proj

---
+++ SampleLib/SampleLib/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleLib
+{
+    public class Student
+    {
+        private readonly string name;
+
+        public Student(string Name)
+        {
+            name = Name;
+        }
+
+        public string Name { get; set; }
+    }
+}
